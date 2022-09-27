1455aa237fc716269983b479b4742a14be915ca9
Salman Alfast <salmanalfast@gmail.com>
2022-09-27 09:30:05 +0800

    Add files via upload

---
+++ root/js/scripts.js
@@ -0,0 +1,72 @@
+let slideIndext = 1;
+showSlides(slideIndext);
+
+function plusSlides(n) {
+    showSlides(slideIndext += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndext = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementByClassName("mySlides");
+    let dots = document.getElementByClassName("dot");
+    if (n > slides.length) {slideIndext = 1}
+    if (n < 1) {slideIndext = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndext-1].style.display = "block";
+    dots[slideIndext-1].className += " active";
+}
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
